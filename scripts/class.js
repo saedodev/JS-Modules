cd07cdaa86537ebee1c36193a94b8a1e9fd572b2
Ridho <sayidridho65@gmail.com>
2025-08-25 16:05:38 +0800

    [add & rewrite] Variable & class in module as well as multi-export too with a change of file from with-module.html to index.html

---
+++ scripts/class.js
@@ -0,0 +1,10 @@
+export class Orang {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayHello(name) {
+        console.log(`Halo ${name}, kenalin namaku ${this.name}`)
+    }
+}
