eac880506c46cbdd6293ed9744051f5b3019916b
Ridho <sayidridho65@gmail.com>
2025-08-26 16:37:08 +0800

    [add] Using Module Object for importing all variable, class or function

---
+++ scripts/module-object.js
@@ -0,0 +1,20 @@
+export const firstName = "Sayid";
+export const surName = "Ridho";
+export const lastName = "Yahya";
+
+export function nambah(value1, value2){
+    return value1 + value2;
+}
+
+export class Pegawai {
+
+    constructor() {
+        this.name3 = name3;
+    }
+
+    hello() {
+        console.log(`Halo saya ${this.name3}, saya adalah JavaScript Fullstack Programmer`)
+    }
+}
+
+const name3 = "Budi";
